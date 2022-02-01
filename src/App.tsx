82182a210e5459998f3d3617008c57cd936aa8d5
unknown <sonja.armio@gmail.com>
2022-02-01 20:55:51 +0200

    Toiminnallisuuksia rakennettu

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import InputField from './components/InputField/InputField';
+import InputField from './components/InputField';
+import TodoList from './components/TodoList';
+import { Todo } from './model';
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>('');
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo('');
+    }
+  };
+
+  console.log(todo);
+
   return (
     <div className='App'>
       <span className='heading'>ToDo</span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {todos.map((t) => (
+        <li>{t.todo}</li>
+      ))}
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 };
